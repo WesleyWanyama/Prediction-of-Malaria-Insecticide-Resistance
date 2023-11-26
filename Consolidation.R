7d08cb0e57d99da379f615471d5e4a970f4e3aa5
Wesley Wanyama <83776940+WesleyWanyama@users.noreply.github.com>
2023-11-26 16:02:20 +0300

    Start Milestone 5

---
+++ Consolidation.R
@@ -0,0 +1,18 @@
+# *****************************************************************************
+# Plumber API ----
+# Install and Load the Required Packages ----
+## plumber ----
+if (require("plumber")) {
+  require("plumber")
+} else {
+  install.packages("plumber", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## caret ----
+if (require("caret")) {
+  require("caret")
+} else {
+  install.packages("caret", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
