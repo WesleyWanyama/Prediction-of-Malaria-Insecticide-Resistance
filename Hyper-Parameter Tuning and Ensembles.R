3d583d0f83cf5103e79cb8486ee4e14c08f9e6eb
Wesley Wanyama <83776940+WesleyWanyama@users.noreply.github.com>
2023-11-26 11:49:44 +0300

    Start Milestone 4

---
+++ Hyper-Parameter Tuning and Ensembles.R
@@ -0,0 +1,36 @@
+# *****************************************************************************
+#Hyperparameter Tuning ----
+#Install and Load the Required Packages ----
+## randomForest ----
+if (require("randomForest")) {
+  require("randomForest")
+} else {
+  install.packages("randomForest", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## mlbench ----
+if (require("mlbench")) {
+  require("mlbench")
+} else {
+  install.packages("mlbench", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## caret ----
+if (require("caret")) {
+  require("caret")
+} else {
+  install.packages("caret", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## RRF ----
+if (require("RRF")) {
+  require("RRF")
+} else {
+  install.packages("RRF", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+# STEP 3. Train the Model ----
